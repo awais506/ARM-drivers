0358e961b08dead4fb4087fb4ad8996ae90e4272
awais506 <m.awaisaas@gmail.com>
2016-08-28 04:34:11 +0500

    Update UART.c

--- ADC/UART.c
+++ ADC/UART.c
@@ -2,24 +2,7 @@
 // Runs on TM4C123 or LM4F120
 // Lab 11 involves switching this from UART1 to UART0.
 //                 switching from PC5,PC4 to PA1,PA0
-// Daniel Valvano
-// December 29, 2014
 
-/* This example accompanies the book
-   "Embedded Systems: Introduction to ARM Cortex M Microcontrollers",
-   ISBN: 978-1469998749, Jonathan Valvano, copyright (c) 2014
-
- Copyright 2015 by Jonathan W. Valvano, valvano@mail.utexas.edu
-    You may use, edit, run or distribute this file
-    as long as the above copyright notice remains
- THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
- OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
- VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
- OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
- For more information about my classes, my research, and my books, see
- http://users.ece.utexas.edu/~valvano/
- */
 
 // this connection occurs in the USB debugging cable
 // U0Rx (PA0) connected to serial port on PC
